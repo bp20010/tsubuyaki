66ebc372619a5bfea1ae5f222fdf97574f4fc4ee
bp20010 <bp20010@sic.shibaura-it.ac.jp>
2021-12-04 13:02:02 +0900

    update

---
+++ src/main/java/tsubuyaki/views/list/ListView.java
@@ -0,0 +1,39 @@
+package tsubuyaki.views.list;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+
+
+@PageTitle("tsubuyaki")
+@Route(value = "empty")
+@RouteAlias(value = "")
+public class ListView extends VerticalLayout {
+
+    public ListView() {
+        VerticalLayout tsubuyaki= new VerticalLayout();
+        TextField nameField=new TextField("");
+        TextField contextField=new TextField("");
+        Button button=new Button("Tsubuyaku");
+        button.addClickListener(click->{
+            add(new Label(nameField.getValue()+":"+contextField.getValue()));
+        });
+        add(
+            new H1("tsubuyaki"),
+            tsubuyaki,
+            new HorizontalLayout(
+                nameField,
+                contextField,
+                button
+            )
+        );
+        
+    }
+
+}
